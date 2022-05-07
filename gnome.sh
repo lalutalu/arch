915efe367b1eef97fa4003424268268e810f5697
Lukas Alutalu <98784748+lalutalu@users.noreply.github.com>
2022-05-07 10:02:49 -0400

    Create gnome.sh

---
+++ gnome.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+sudo reflector -c Canada -a 6 --sort rate --save /etc/pacman.d/mirrorlist
+
+
+git clone https://aur.archlinux.org/paru.git
+cd paru/
+makepkg -si PKGBUILD --noconfirm
+
+#pikaur -S --noconfirm system76-power
+#sudo systemctl enable --now system76-power
+#sudo system76-power graphics integrated
+#pikaur -S --noconfirm gnome-shell-extension-system76-power-git
+#pikaur -S --noconfirm auto-cpufreq
+#sudo systemctl enable --now auto-cpufreq
+
+sudo pacman -S --noconfirm xorg gdm gnome gnome-extra firefox gnome-tweaks simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+
+sudo flatpak install -y spotify
+sudo flatpak install -y discord
+
+sudo systemctl enable gdm
+/bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
+sleep 5
+sudo reboot
